253d3baa15792959063314fec99cccd7fb47b9d9
kalim332 <kalim332@newschool.edu>
2022-03-02 02:48:16 -0500

    Update

--- Studio/World Building V5/jquery.js
+++ Studio/World Building V5/jquery.js
@@ -1,16 +1,12 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
 
 
