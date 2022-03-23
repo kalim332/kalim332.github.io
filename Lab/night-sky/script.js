be17959cf370989377dcf9baf7cf19ae467d7332
kalim332 <kalim332@newschool.edu>
2022-03-23 05:42:24 -0400

    shooting star

--- Lab/night-sky/script.js
+++ Lab/night-sky/script.js
@@ -1,16 +1,39 @@
-let bubbles = document.querySelectorAll('.bubble');
+let stars = document.querySelectorAll('.star');
 let container = document.querySelector('#container');
+let moon = document.querySelector('#moon');
+// let shoot = document.querySelector('.shoot');
+// let shootbefore = document.querySelector('.shoot::before');
+
+var anim = document.querySelectorAll('.shoot');
 
 for (let i = 0; i < 800; i++) {
-  let bubble = document.createElement('div');
-  bubble.classList.add('bubble');
-  bubble.style.left = (99 * Math.random()) + '%';
-  bubble.style.top = (99 * Math.random()) + '%'; 
-  container.appendChild(bubble);
+  let star = document.createElement('div');
+  star.classList.add('star');
+  star.style.left = (99 * Math.random()) + '%';
+  star.style.top = (99 * Math.random()) + '%'; 
+  container.appendChild(star);
 } 
 
-container.addEventListener('click', function (event) {
-  if (event.target.classList.contains('bubble')) {
-    event.target.remove();
-  }
-});
+// container.addEventListener('click', function (event) {
+//   if (event.target.classList.contains('moon')) {
+//     event.target.remove();
+
+// moon.onclick = () => shootbefore.classList.add('shoot');(
+  
+  // container.addEventListener('click', function (event) {
+  //   if (event.target.classList.contains('moon')) {
+  //     event.target.element.remove('animationplaystate');
+  //   } 
+  // }
+  
+  
+  document.getElementById('moon').onclick = function () {
+    for (var i = 0; i < anim.length; i++) {
+        if (anim[i].style.animationPlayState == 'paused') {
+            anim[i].style.animationPlayState = 'running';
+        }
+        else {
+            anim[i].style.animationPlayState = 'paused';
+        }
+    }
+}
