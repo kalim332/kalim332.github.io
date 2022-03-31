4c1d7a6dd6ac0636ef93685d676135303f043a5b
kalim332 <kalim332@newschool.edu>
2022-03-31 13:46:43 -0400

    Bookstore initial

---
+++ Lab/jquery-demo/jquery.js
@@ -0,0 +1,33 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[hello earthlings]"); })
+
+// JS files comment with two backslash lines 
+
+$(document).ready(function() {
+    $("#bananas").hover(function(){
+     $("#ufo").show(); }); })
+
+    
+$(document).ready(function() {
+    $("#cat").click(function(){
+    $("#diamond").toggle(); }); 
+    })
+
+
+$(document).ready(function() {
+    $("#grapes").hover(function(){
+            $("#earthling").css("border-color", "yellow");
+                });
+    })
+
+
+$(document).ready(function()  {
+    $("#diamond").hover(function(){
+            $("#earthling").append("we come in peach");
+                });
+    })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+
+$(document).ready(function() {$("#bananas").draggable(); })
