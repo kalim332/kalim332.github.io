123ff3fe887e124cfc14b126a1a81973246a7f39
kalim332 <kalim332@newschool.edu>
2022-05-06 17:58:31 -0400

    laundry boundary initial

---
+++ Studio/Laundry+Boundry/jquery.js
@@ -0,0 +1,17 @@
+var links = [
+              "google.com",
+              "fxe.html"
+              ]
+
+            function openSite() {
+              // get a random number between 0 and the number of links
+              var randIdx = Math.random() * links.length;
+              // round it, so it can be used as array index
+              randIdx = parseInt(randIdx, 10);
+              // construct the link to be opened
+              var link = 'http://' + links[randIdx];
+              return link;
+              };
+             
+    
+    document.getElementById("link").innerHTML = openSite();
